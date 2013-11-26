3d7162fb8ad661e0f533e3664955c3c952e7e903
Ricardo <ei09045@fe.up.pt>
2013-11-26 02:06:36 +0000

    Push with initial code

--- src/com/example/cmov2/MainActivity.java
+++ src/com/example/cmov2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cmov2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	}
 
 	@Override
